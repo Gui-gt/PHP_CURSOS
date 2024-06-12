e0672c2bc831a3d4dfd35d3d106d871f91cb344c
Gui-gt <30755guilherme@gmail.com>
2024-06-12 11:54:25 -0300

    att

--- Guanabara/ex003/index.php
+++ Guanabara/ex003/index.php
@@ -4,14 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP</title>
+    <title>Info</title>
 </head>
 
 <body>
     <?php 
-date_default_timezone_set("america/sao_paulo");// GMT-3;
-echo "hoje é dia" .date("d/s/y");
-echo "agora são" .date("G:i");
+    phpinfo();
+
 ?>
 </body>
 
