c92930fa79e997447faf2d014afadc42ce0b4ab1
Gui-gt <30755guilherme@gmail.com>
2024-06-13 10:33:30 -0300

    Update index.php

--- Guanabara/ex004/index.php
+++ Guanabara/ex004/index.php
@@ -1,49 +1,12 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Document</title>
 </head>
 
-<body>
-
-    <h2>Somando valores</h2>
-    <?php 
-
-    
-    $n1 = 1;
-    $n2 = 2;
-
-    $res = $n1 + $n2;
-
-    echo "$n1 + $n2 é $res"
-
-    if($cpf.length != 11 )
-    return false;
-
-
-    if(new string ($cpf[0], $cpf.length) == $cpf)
-    return false;
-
-
-    $soma = 0;
-
-    for($i = 0;$i < 9;$i++)
-    $soma += int.parse($cpf[0].Tostring()) * (10 - $i);
-    $resto = $soma - 11;
-    $digitoverificador1 = $resto < 2 ? 0 : 11 - $resto;
-    if($digitoverificador1 != parse_str($cpf[9].Tostring()))
-    return false;
-
-    const PAIS = "brasil";
-    $numero = 10;
-
-    echo "hello word"
-
-    
-    ?>
-</body>
+<body> </body>
 
 </html>
